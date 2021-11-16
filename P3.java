78f967dbb4a0db791bf418404092c31e689a9bd8
GamuchiraiS <80484981+GamuchiraiS@users.noreply.github.com>
2021-11-16 12:18:03 +0200

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,27 @@
+//importing utility classes
+import java.util.*;
+
+public class P3{
+	public static void main(String args []){
+		//From the Fibonacci sequence the first and second
+        //number are always going to be 
+        //0 and 1 so we code that
+        int firstNum = 0;
+        int secondNum = 1;
+        int thirdNum;
+        int numsShown;
+
+        //Creating a scanner object
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers from the Fibonacci series must be shown? ");
+        numsShown = sc.nextInt();
+        System.out.println(firstNum + "\n" + secondNum); //prints 1st and 2nd num
+
+        for (int i = 2; i < numsShown; i++){
+        	thirdNum = firstNum + secondNum;
+        	System.out.println(thirdNum + "");
+        	firstNum = secondNum;
+        	secondNum = thirdNum;
+        }
+	}
+}
